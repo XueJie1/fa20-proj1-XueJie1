398c92321181efd7ceb395033ae423e3842df8b0
XueJie1 <3124792285@qq.com>
2024-07-13 09:32:58 +0800

    Complete Part A

--- imageloader.c
+++ imageloader.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **              Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				XueJie1
 **
 **
 ** DATE:        2020-08-15
@@ -20,22 +20,109 @@
 #include <string.h>
 #include "imageloader.h"
 
-//Opens a .ppm P3 image file, and constructs an Image object. 
-//You may find the function fscanf useful.
-//Make sure that you close the file with fclose before returning.
-Image *readData(char *filename) 
+// Opens a .ppm P3 image file, and constructs an Image object.
+// You may find the function fscanf useful.
+// Make sure that you close the file with fclose before returning.
+Image *readData(char *filename)
 {
-	//YOUR CODE HERE
-}
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening file '%s'\n", filename);
+        return NULL;
+    }
 
-//Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
+    Image *img = malloc(sizeof(Image));
+    if (img == NULL) {
+        fclose(fp);
+        return NULL;
+    }
+
+    char format[3];
+    if (fscanf(fp, "%2s", format) != 1 || strcmp(format, "P3") != 0) {
+        fprintf(stderr, "Invalid file format\n");
+        fclose(fp);
+        free(img);
+        return NULL;
+    }
+
+    if (fscanf(fp, "%d %d", &img->cols, &img->rows) != 2) {
+        fprintf(stderr, "Error reading dimensions\n");
+        fclose(fp);
+        free(img);
+        return NULL;
+    }
+
+    int maxval;
+    if (fscanf(fp, "%d", &maxval) != 1 || maxval != 255) {
+        fprintf(stderr, "Invalid max color value\n");
+        fclose(fp);
+        free(img);
+        return NULL;
+    }
+
+    // Allocate memory for image data
+    img->image = malloc(img->rows * sizeof(Color *));
+    for (uint32_t i = 0; i < img->rows; i++) {
+        img->image[i] = malloc(img->cols * sizeof(Color));
+    }
+
+    // Read pixel data
+    for (uint32_t i = 0; i < img->rows; i++) {
+        for (uint32_t j = 0; j < img->cols; j++) {
+            if (fscanf(fp, "%hhu %hhu %hhu", 
+                       &img->image[i][j].R, 
+                       &img->image[i][j].G, 
+                       &img->image[i][j].B) != 3) {
+                fprintf(stderr, "Error reading pixel data\n");
+                // Clean up and return NULL
+            }
+        }
+    }
+
+    fclose(fp);
+    return img;
+}
+// Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	// YOUR CODE HERE
+	if (image == NULL)
+	{
+		fprintf(stderr, "Empty image object\n");
+		return;
+	}
+
+	printf("P3\n");
+	printf("%d %d\n255\n", image->cols, image->rows);
+	for (int row_count = 0; row_count < image->rows; row_count++)
+	{
+		for (int col_count = 0; col_count < image->cols; col_count++)
+		{
+			if (col_count != image->cols - 1)
+				printf("% 3hhu % 3hhu % 3hhu   ",
+					   image->image[row_count][col_count].R, image->image[row_count][col_count].G, image->image[row_count][col_count].B);
+			else
+				printf("% 3hhu % 3hhu % 3hhu",
+					   image->image[row_count][col_count].R, image->image[row_count][col_count].G, image->image[row_count][col_count].B);
+		}
+		printf("\n");
+	}
 }
 
-//Frees an image
+// Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	// YOUR CODE HERE
+	if (image)
+	{
+		if (image->image)
+		{
+			for (uint32_t i = 0; i < image->rows; i++)
+			{
+				free(image->image[i]);
+			}
+			free(image->image);
+		}
+		free(image);
+	}
 }
--- steganography.c
+++ steganography.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **				Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				XueJie1
 **
 ** DATE:        2020-08-23
 **
@@ -22,12 +22,75 @@
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	Color *one_pixel = malloc(sizeof(Color));
+	uint8_t B = image->image[row][col].B;
+	uint8_t least_bit = B & 1;
+	// if the least bit is 1, color is white, else is black
+	if(least_bit) {
+		one_pixel->R = 255;
+		one_pixel->G = 255;
+		one_pixel->B = 255;
+	} else {
+		one_pixel->R = 0;
+		one_pixel->G = 0;
+		one_pixel->B = 0;
+	}
+
+	return one_pixel;
 }
 
-//Given an image, creates a new image extracting the LSB of the B channel.
+// Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+    if (image == NULL) {
+        return NULL;
+    }
+
+    Image *decrypted = malloc(sizeof(Image));
+    if (decrypted == NULL) {
+        return NULL;
+    }
+
+    decrypted->rows = image->rows;
+    decrypted->cols = image->cols;
+
+    decrypted->image = malloc(sizeof(Color *) * decrypted->rows);
+    if (decrypted->image == NULL) {
+        free(decrypted);
+        return NULL;
+    }
+
+    for (int i = 0; i < decrypted->rows; i++) {
+        decrypted->image[i] = malloc(sizeof(Color) * decrypted->cols);
+        if (decrypted->image[i] == NULL) {
+            // Free previously allocated rows
+            for (int j = 0; j < i; j++) {
+                free(decrypted->image[j]);
+            }
+            free(decrypted->image);
+            free(decrypted);
+            return NULL;
+        }
+    }
+
+    for (int row_count = 0; row_count < decrypted->rows; row_count++) {
+        for (int col_count = 0; col_count < decrypted->cols; col_count++) {
+            Color *pixel = evaluateOnePixel(image, row_count, col_count);
+            if (pixel == NULL) {
+                // Handle error: free allocated memory and return NULL
+                for (int i = 0; i < decrypted->rows; i++) {
+                    free(decrypted->image[i]);
+                }
+                free(decrypted->image);
+                free(decrypted);
+                return NULL;
+            }
+            decrypted->image[row_count][col_count] = *pixel;
+            free(pixel);  // Free the memory allocated by evaluateOnePixel
+        }
+    }
+
+    return decrypted;
 }
 
 /*
@@ -46,4 +109,27 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if (argc < 1) {
+		fprintf(stderr, "Argument needed\n");
+		return -1;
+	}
+	char *filename = argv[1];
+	Image *image = readData(filename);
+	free(filename);
+	if (image == NULL){
+		fprintf(stderr, "Failed to read from file\n");
+		return -1;
+	}
+	
+	Image *new_image = steganography(image);
+	free(image);
+	if (new_image == NULL) {
+		fprintf(stderr, "Cannot convert image\n");
+		return -1;
+	}
+
+	writeData(new_image);
+	free(new_image);
+	return 0;
+
 }
